62f301f5acd4cecee7b005794ab1e7f58643fa81
Marcus Harris <79154965+Marcus-Harris@users.noreply.github.com>
2021-07-08 16:59:15 -0400

    third commit

--- src/Components/authentication-button.js
+++ src/Components/authentication-button.js
@@ -1,11 +1,18 @@
-// src/components/authentication-button.js
 import React from "react";
 import LoginButton from "./login-button";
 import LogoutButton from "./logout-button";
-import { useAuth0 } from "@auth0/auth0-react";
-const AuthenticationButton = () => {
-    const { isAuthenticated } = useAuth0();
-    return isAuthenticated ? <LogoutButton /> : <LoginButton />;
-};
+import { withAuth0 } from "@auth0/auth0-react";
+import SignupButton from "./signup-button";
 
-export default AuthenticationButton;
+class AuthenticationButton extends React.Component {
+  render() {
+    const { isAuthenticated } = this.props.auth0;
+    return isAuthenticated ? <LogoutButton /> : 
+    <span>
+      <LoginButton className="inline" id="floatleft"/>
+      <SignupButton className="inline" id="floatright"/>
+    </span>;
+  }
+}
+
+export default withAuth0(AuthenticationButton);
--- src/Components/signup-button.js
+++ src/Components/signup-button.js
@@ -1,6 +1,7 @@
 // src/components/signup-button.js
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+
 const SignupButton = () => {
     const { loginWithRedirect } = useAuth0();
     return (
