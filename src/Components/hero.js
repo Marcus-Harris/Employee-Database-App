9d0638c04c8860fbbd3bb4494b7fcbe2b5de40df
ibtxhiskhang <ibtxhiskhang@gmail.com>
2021-07-13 11:56:14 -0400

    adding header image to home page

--- src/Components/hero.js
+++ src/Components/hero.js
@@ -4,26 +4,26 @@ class Hero extends React.Component {
     logo = "https://cdn.auth0.com/blog/auth0-react-sample/assets/logo.png";
     render() {
         return (
-            <div className="text-center hero">
-                <img
-                    className="mb-3 app-logo"
-                    src={this.logo}
-                    alt="React logo"
-                    width="120"
-                />
-                <h1 className="mb-4">React Sample Project</h1>
-                <p className="lead">
-                    This is a sample application that demonstrates an authentication flow
-                    for an SPA, using{" "}
-                    <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href="https://auth0.com/docs/quickstart/spa/react"
-                    >
-                        React
-                    </a>
-                </p>
-            </div>
+            <div class="winportant">
+        <h2>WINPORTANT INFORMATION</h2> 
+                <div class="news">
+                    
+                    <img src="https://3chxux42u1m943cnnj18o850-wpengine.netdna-ssl.com/wp-content/uploads/2019/07/grid-img@2x-1024x905.jpg" class="image"></img>
+                    <br></br>
+                    <div class="paragraph">
+                        <div class="text"><p>Starting with about 36 cohort members, the program will end with their most 
+                        successful group as of today! Their ability to help aid in their members’ needs
+                        and the resources that has helped these individuals become successful is truly
+                        amazing. All 36 cohorts will begin their careers in the job placement that the
+                        program has provided in early August. They will continue to build their career
+                        as software developers, data analysts, UX/UI designers, testers, and many more.
+                        </p>
+                        <button> Find Out More</button>
+                </div>
+                </div>
+                </div>
+                </div>
+
         );
     }
 }
--- src/views/profile.js
+++ src/views/profile.js
@@ -7,25 +7,37 @@ class Profile extends React.Component {
         const { name, picture, email } = user;
         return (
             <div>
-                <div className="row align-items-center profile-header">
-                    <div className="col-md-2 mb-3">
+            <div className="col-md-2 mb-3">
                         <img
                             src={picture}
                             alt="Profile"
                             className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
-                        />
-                    </div>
-                    <div className="col-md text-center text-md-left">
+                        />   
+            <div className="col-md text-center text-md-left">
                         <h2>{name}</h2>
                         <p className="lead text-muted">{email}</p>
-                    </div>
-                </div>
+                    </div></div>
+
+<div class="card-info">
+<h2> Welcome! Here is your card information! </h2>
+<img src="https://www.w3schools.com/howto/img_avatar2.png" height="200px" width="200" align="left"></img>
+
                 <div className="row">
                     <pre className="col-12 text-light bg-dark p-4">
                         {JSON.stringify(user, null, 2)}
                     </pre>
                 </div>
-            </div>
+
+</div>
+<br></br><br></br><br></br>
+                <div class="options" display="block">
+                    <div class="option-card"><img src="https://www.shareicon.net/data/512x512/2015/11/26/678251_personal_512x512.png" height="50px" width="50px"></img><br></br>Edit Card Information</div>
+                    <div class="option-card"><img src="https://static.thenounproject.com/png/209890-200.png" height="50px" width="50px"></img><br></br>View Paystubs</div>
+                    <div class="option-card"><img src="https://img.icons8.com/ios/452/time-card.png" height="50px" width="50px"></img><br></br>Edit Time Card</div>
+                    <div class="option-card"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOA_kmm5_a4KltFWmCcVB8BgvGl8WQytZEMw&usqp=CAU" height="50px" width="50px"></img><br></br>View Benefits</div>
+                    <div class="option-card"><img src="https://static.thenounproject.com/png/158275-200.png" height="50px" width="50px"></img><br></br>View Team</div>
+                </div>
+</div>
         );
     }
 }
