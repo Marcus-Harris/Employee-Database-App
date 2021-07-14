1de7b5e7f633bf650e2a22b3e0290f46be466398
ibtxhiskhang <ibtxhiskhang@gmail.com>
2021-07-14 15:55:35 -0400

    Update Profile Information

--- src/views/profile.js
+++ src/views/profile.js
@@ -2,6 +2,9 @@ import React, { Fragment } from "react";
 import { withAuth0 } from "@auth0/auth0-react";
 import { HomeContent } from "../Components";
 
+
+
+
 class Profile extends React.Component {
     render() {
         const { user } = this.props.auth0;
@@ -21,20 +24,35 @@ class Profile extends React.Component {
                         <p className="lead text-muted" id="profileemail">{email}</p>
                     </div></div>
 
-
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="sha384-wESLQ85D6gbsF459vf1CiZ2+rr+CsxRY0RpiF1tLlQpDnAgg6rwdsUF1+Ics2bni" crossorigin="anonymous"></link>
 <div class="card-info">
 <h2 id="win"> Welcome! Here is your card information! </h2>
+
+
 <img src="https://www.w3schools.com/howto/img_avatar2.png" class="profileimage"></img>
 
                 <div className="row">
-                    <pre className="col-12 text-light bg-dark p-4">
-                        {JSON.stringify(user, null, 2)}
+                    
+                    <pre className="information">
+                        {/* {JSON.stringify(user, null, 2)} */}
+                        <a href="http://www.google.com" target="_blank"><input type="button" class="editcard" value="Edit Card" /></a>
+
+                        <br></br><br></br>
+                        <h6>Name:</h6> {user.name}
+                        <br></br><br></br>
+                        <h6>nickname:</h6>{user.nickname}
+                        <br></br><br></br>
+                        <h6>email:</h6>{user.email}
+                        <br></br><br></br>
+                        <h6>phone number:</h6> {user.phone}
+
                     </pre>
                 </div>
                 </div>
                 <br></br>
                 <br></br>
                 <br></br>
+                <hr></hr>
 
 <Fragment>
 <HomeContent />
